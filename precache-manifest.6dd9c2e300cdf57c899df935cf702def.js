56d6404d3a022d349f5b9deca288ec7795a5b757
Yousuff khan <youskhan@cisco.com>
2020-04-17 14:17:19 +0530

    Updates

---
+++ precache-manifest.6dd9c2e300cdf57c899df935cf702def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad23ab10155d791d7aeb8f4adaa495ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b623e3a788e7af602cf",
+    "url": "/monsters-rolodex/static/css/main.de8a84f4.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b623e3a788e7af602cf",
+    "url": "/monsters-rolodex/static/js/main.ae24f536.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
